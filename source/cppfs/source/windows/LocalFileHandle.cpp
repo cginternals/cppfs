29c6686ed68f2f9629f27c23fb79ecb7a3c5c332
Karl Wallner <kwallner@mail.de>
2018-02-11 18:53:38 +0100

    Switched parameters for Windows-CreateHardLinkA.

--- source/cppfs/source/windows/LocalFileHandle.cpp
+++ source/cppfs/source/windows/LocalFileHandle.cpp
@@ -312,7 +312,7 @@ bool LocalFileHandle::createLink(AbstractFileHandleBackend & dest)
     }
 
     // Copy file
-    if (!CreateHardLinkA(src.c_str(), dst.c_str(), 0))
+    if (!CreateHardLinkA(dst.c_str(), src.c_str(), 0))
     {
         // Error!
         return false;
