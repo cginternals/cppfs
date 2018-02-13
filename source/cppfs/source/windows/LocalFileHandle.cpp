5d9bb60fc168494ac0978399d5f8a7904de6ed8f
Karl Wallner <kwallner@mail.de>
2018-02-14 00:06:51 +0100

    Switched parameters for Windows-CreateSymbolicLinkA.

--- source/cppfs/source/windows/LocalFileHandle.cpp
+++ source/cppfs/source/windows/LocalFileHandle.cpp
@@ -338,7 +338,7 @@ bool LocalFileHandle::createSymbolicLink(AbstractFileHandleBackend & dest)
     }
 
     // Copy file
-    if (!CreateSymbolicLinkA(src.c_str(), dst.c_str(), 0))
+    if (!CreateSymbolicLinkA(dst.c_str(), src.c_str(), 0))
     {
         // Error!
         return false;
