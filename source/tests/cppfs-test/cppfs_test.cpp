3f50c49924e86094e58addffb98123e20983f2d9
fireuse <a.topolski.at@gmail.com>
2020-05-31 20:30:33 +0200

    Added SHA1
    Signed-off-by: fireuse <a.topolski.at@gmail.com>

--- source/tests/cppfs-test/cppfs_test.cpp
+++ source/tests/cppfs-test/cppfs_test.cpp
@@ -1,5 +1,6 @@
 
 #include <gmock/gmock.h>
+#include "../../cppfs/source/SHA1.hpp"
 
 
 class cppfs_test: public testing::Test
@@ -11,7 +12,10 @@ public:
 TEST_F(cppfs_test, CheckSomeResults)
 {
     // [TODO] Implement test
-
+    std::stringstream t1("");
+    std::stringstream t2("abc");
+    EXPECT_EQ(sha1(t1),"da39a3ee5e6b4b0d3255bfef95601890afd80709");
+    EXPECT_EQ(sha1(t2),"a9993e364706816aba3e25717850c26c9cd0d89d");
     EXPECT_EQ((unsigned int) 0, 0);
     // ...
 }
